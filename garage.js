02fc3599993d2522faced764be2dc8d6380c8895
Katrinqe <96010680+Katrinqe@users.noreply.github.com>
2025-12-23 16:28:36 +0100

    Update garage.js

--- garage.js
+++ garage.js
@@ -1,7 +1,4 @@
 alert("HALLO! ICH BIN DA!");
-alert("DATEI GELADEN!"); // Test 1: Wird die Datei überhaupt gelesen?
-
-
 
 function safeLoad(key) { 
     // ... hier kommt dein normaler Code von vorhin ...
