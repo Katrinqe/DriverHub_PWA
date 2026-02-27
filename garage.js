951c94ce81124c225fcb254f25ec8ca4fb229a01
Katrinqe <96010680+Katrinqe@users.noreply.github.com>
2026-02-27 02:50:15 +0100

    Update garage.js

--- garage.js
+++ garage.js
@@ -265,8 +265,8 @@ window.GarageLogic = {
                     <img src="${mod.logo}" alt="${mod.name}">
                 </div>
                 
-              <div class="model-3d-box">
-                    <model-viewer src="${mod.file}" camera-orbit="40deg 85deg auto" field-of-view="12deg" disable-zoom disable-pan shadow-intensity="1.5" exposure="1" interaction-prompt="none" style="width:100%; height:100%;"></model-viewer>
+         <div class="model-3d-box">
+                    <model-viewer src="${mod.file}" camera-orbit="40deg 85deg auto" disable-zoom disable-pan shadow-intensity="1" exposure="1" interaction-prompt="none" style="width:100%; height:100%;"></model-viewer>
                 </div>
                 
                 <div class="model-specs-row">
