c2add07d60829b5054c17342e3e3200c7ff04332
Katrinqe <96010680+Katrinqe@users.noreply.github.com>
2026-03-19 14:59:41 +0100

    Update map.js

--- map.js
+++ map.js
@@ -97,12 +97,12 @@ document.addEventListener('DOMContentLoaded', () => {
             interactive: false, // User kann nicht scrollen/zoomen/drehen
             attributionControl: false // Die Map-Zuweisung unten rechts ausblenden
         });
-// Optischen Mittelpunkt verschieben (für die Buttons rechts)
-        // Wir täuschen vor, dass die Karte rechts ein unsichtbares Menü von 100px hat.
-        // Dadurch schiebt MapLibre den eigenen Mittelpunkt automatisch 50px nach links.
+
+        // Da die Buttons jetzt unten liegen, geben wir der Karte ein virtuelles
+        // Padding von unten, damit der User-Dot ein Stück nach oben wandert.
         libreMap.on('load', () => {
             libreMap.jumpTo({
-                padding: { right: 100, bottom: 20 }
+                padding: { bottom: 50 } 
             });
         });
         // 3. Einen Marker für den Standort hinzufügen (wenn wir einen haben)
